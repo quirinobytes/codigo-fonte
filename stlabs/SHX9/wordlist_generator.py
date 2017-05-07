da15e3437b997ac0b28f26ef9ef6eaad62cb4159
Rafael Quirino <quirinobytes@gmail.com>
2017-05-07 18:52:27 -0300

    Melhorias

--- stlabs/SHX9/wordlist_generator.py
+++ stlabs/SHX9/wordlist_generator.py
@@ -7,4 +7,4 @@ hex_chars = '0123456789abcdef'
 wordlist = open('wordlist', 'a')
 
 for words in itertools.product(hex_chars, repeat=5):
-	wordlist.write(''.join(words) + 'shx' + '\n')
+	wordlist.write( 'shx' + ''.join(words) + '\n')
