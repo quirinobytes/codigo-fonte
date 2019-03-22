3fac6ef3a1022e48f63589d986f8cba61a7f7fa1
Rafael Castro <quirinobytes@gmail.com>
2019-03-22 14:15:07 -0300

    Commiting my job!

--- node.js/https-exemplo/index.js
+++ node.js/https-exemplo/index.js
@@ -1,12 +1,25 @@
-const https = require('https');
-const fs = require('fs');
+var express = require('express')
+var https = require('https')
+var http = require('http')
+var fs = require('fs')
+var app = express()
 
 const options = {
   key: fs.readFileSync('keys/key.pem'),
   cert: fs.readFileSync('keys/cert.pem')
-};
+}
 
-https.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end('hello world\n');
-}).listen(443);
+app.get('/http', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('hello world\n')
+  res.end('carregado HTTP')
+})
+
+app.get('/https', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('hello world\n')
+  res.end('carregado HTTPS')
+})
+
+http.createServer(app).listen(80)
+https.createServer(options, app).listen(443)
