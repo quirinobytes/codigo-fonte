ff771d9cc79996d2a1bfa3ab0af9e2faf28cbf0d
Rafael Castro <quirinobytes@gmail.com>
2019-03-22 23:29:12 -0300

    Commiting my job!

--- node.js/https-exemplo/index.js
+++ node.js/https-exemplo/index.js
@@ -11,13 +11,11 @@ const options = {
 
 app.get('/http', function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('hello world\n')
   res.end('carregado HTTP')
 })
 
 app.get('/https', function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('hello world\n')
   res.end('carregado HTTPS')
 })
 
