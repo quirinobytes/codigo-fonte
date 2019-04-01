6823bd8314ce3962f709267ad1a99719edb58538
quirinobytes <quirinobytes@gmail.com>
2019-04-01 20:09:23 -0300

    Commiting my job!

--- node.js/auth-examples/jwt-auth-with-express.Route/routes/controllers/root.js
+++ node.js/auth-examples/jwt-auth-with-express.Route/routes/controllers/root.js
@@ -8,5 +8,10 @@ var root = function (req, res, next) {
   res.send('<h1> Root route </h1>')
 }
 
+var rootPages = function (req, res, next) {
+  console.log('entrou no rootPages')
+  express.static('../../views')
+}
+
 // isso exporta um objeto com os valores de login e config
-module.exports = { root: root }
+module.exports = { root: root, rootPages: rootPages }
